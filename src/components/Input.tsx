4809a8daed182f031983c1ebd185d6ef9317bdcd
X-Tripen <bnhp@op.pl>
2023-03-12 08:45:35 +0100

    react form validation for login form added

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,9 +4,19 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	refs?: React.RefObject<HTMLInputElement>;
 	date?: boolean;
 	placeholder?: string;
+	invalid?: boolean;
+	errorMessage?: string;
 }
 
-function Input({ type, refs, required, date, placeholder }: InputProps) {
+function Input({
+	type,
+	refs,
+	required,
+	date,
+	placeholder,
+	invalid,
+	errorMessage,
+}: InputProps) {
 	let classes =
 		"text-center border-none h-7 md:h-10 px-2 py-1 rounded-[6px] bg-gray-200 dark:bg-slate-800 outline-none  duration-200 flex-1 w-full ";
 	if (date) {
@@ -14,17 +24,21 @@ function Input({ type, refs, required, date, placeholder }: InputProps) {
 	}
 
 	return (
-		<div className=" p-0.5 rounded-lg max-w-sm bg-gradient-to-r from-indigo-500 to-rose-500 dark:from-violet-500 dark:to-cyan-500 self-center">
-			<input
-				className={classes}
-				type={type}
-				ref={refs}
-				required={required}
-				placeholder={placeholder}
-				defaultValue={
-					date ? new Date().toISOString().substring(0, 10) : undefined
-				}
-			/>
+		<div className="">
+			<div className="p-0.5 rounded-lg max-w-sm bg-gradient-to-r from-indigo-500 to-rose-500 dark:from-violet-500 dark:to-cyan-500 self-center">
+				<input
+					className={`${classes} ${invalid ? "border-red-500" : ""}`}
+					type={type}
+					ref={refs}
+					placeholder={placeholder}
+					defaultValue={
+						date ? new Date().toISOString().substring(0, 10) : undefined
+					}
+				/>
+			</div>
+			{invalid && errorMessage && (
+				<p className="text-sm text-red-600 pt-3">{errorMessage}</p>
+			)}
 		</div>
 	);
 }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,20 +1,30 @@
-import { FormEvent } from "react";
+import { useRef, useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 import Input from "./Input";
 import ThemeSwitch from "../components/ThemeSwitch";
-interface LoginFormProps {
-	onSubmit?: (e: FormEvent<HTMLFormElement>) => void;
-	userNameRef: React.RefObject<HTMLInputElement>;
-}
 
-function LoginForm({ onSubmit, userNameRef }: LoginFormProps) {
+function LoginForm() {
+	const [isInputValid, setInputValid] = useState(true);
+	const userNameRef = useRef<HTMLInputElement>(null);
+	const navigate = useNavigate();
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (userNameRef.current?.value.length === 0) {
+			setInputValid(false); // set input validity to false if the input field is empty
+		} else {
+			navigate(`/todos?user=${userNameRef.current?.value}`);
+		}
+	};
+
 	return (
 		<div className="bg-gray-200 dark:bg-gray-800  sm:m-4 py-3 px-6 md:py-6 md:px-12 rounded-2xl max-w-3xl shadow-xl relative  duration-150">
 			<div className="absolute top-2 right-2 sm:top-5 sm:right-5">
 				<ThemeSwitch />
 			</div>
 			<form
-				onSubmit={onSubmit}
+				onSubmit={handleSubmit}
 				className="flex flex-col gap-5 sm:gap-8 md:gap-10 text-center items-center"
 			>
 				<p className="font-bold capitalize text-2xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-rose-600 dark:from-violet-400 dark:to-cyan-400 tracking-wider ">
@@ -27,7 +37,13 @@ function LoginForm({ onSubmit, userNameRef }: LoginFormProps) {
 					this information, we can create your user account in our database.
 					Thank you!
 				</p>
-				<Input placeholder={"Type your username"} refs={userNameRef} required />
+				<Input
+					placeholder={"Type your username"}
+					refs={userNameRef}
+					// add the invalid prop and error message if the input is invalid
+					invalid={!isInputValid}
+					errorMessage={"Please enter a valid username"}
+				/>
 				<Button type={"submit"} primary>
 					LogIn
 				</Button>
