4cda2d360e231ec3ee0c976744328d9440e901de
X-Tripen <hubertpaprocki.dev@gmail.com>
2023-08-21 18:10:50 +0200

    fix:SocialMediaList empty link

--- src/components/SocialMediaList/SocialMediaItem.tsx
+++ src/components/SocialMediaList/SocialMediaItem.tsx
@@ -1,19 +1,23 @@
 import { ReactNode } from "react";
 import React from "react";
 interface SocialMediaItemProps {
-	img: ReactNode;
-	link: string;
+  img: ReactNode;
+  link: string;
+  name: string;
 }
 
-const SocialMediaItem: React.FC<SocialMediaItemProps> = ({ img, link }) => {
-	return (
-		<a
-			className="hover:scale-125 transition duration-300 cursor-pointert text-4xl hover:text-purple-500 hover:dark:text-violet-500"
-			href={link}
-		>
-			{img}
-		</a>
-	);
+const SocialMediaItem: React.FC<SocialMediaItemProps> = ({
+  img,
+  link,
+  name,
+}) => {
+  return (
+    <li className="hover:scale-125 transition duration-300 cursor-pointert text-4xl hover:text-purple-500 hover:dark:text-violet-500">
+      <a href={link} aria-label={name}>
+        {img}
+      </a>
+    </li>
+  );
 };
 
 export default SocialMediaItem;
--- src/components/SocialMediaList/SocialMediaList.tsx
+++ src/components/SocialMediaList/SocialMediaList.tsx
@@ -3,48 +3,50 @@ import SocialMediaItem from "./SocialMediaItem";
 import { ReactNode } from "react";
 import React from "react";
 interface ListedSocialMedia {
-	id: number;
-	name: string;
-	icon: ReactNode;
-	link: string;
+  id: number;
+  name: string;
+  icon: ReactNode;
+  link: string;
 }
 
 const ListedSocialMedias: ListedSocialMedia[] = [
-	{
-		id: 1,
-		name: `Facebook`,
-		icon: <RiFacebookFill />,
-		link: `https://www.facebook.com/hbrt.pa/`,
-	},
-	{
-		id: 2,
-		name: `Github`,
-		icon: <RiGithubFill />,
-		link: `https://github.com/X-Tripen`,
-	},
-	{
-		id: 3,
-		name: `LinkedIn`,
-		icon: <RiLinkedinFill />,
-		link: `https://www.linkedin.com/in/hubert-paprocki-25b618236/`,
-	},
+  {
+    id: 1,
+    name: `Facebook`,
+    icon: <RiFacebookFill />,
+    link: `https://www.facebook.com/hbrt.pa/`,
+  },
+  {
+    id: 2,
+    name: `Github`,
+    icon: <RiGithubFill />,
+    link: `https://github.com/X-Tripen`,
+  },
+  {
+    id: 3,
+    name: `LinkedIn`,
+    icon: <RiLinkedinFill />,
+    link: `https://www.linkedin.com/in/hubert-paprocki-25b618236/`,
+  },
 ];
 
 const SocialMediaList = () => {
-	const renderedSocialMediaList = ListedSocialMedias.map((socialMedia) => {
-		return (
-			<SocialMediaItem
-				key={socialMedia.id}
-				img={socialMedia.icon}
-				link={socialMedia.link}
-			></SocialMediaItem>
-		);
-	});
-	return (
-		<div className="flex flex-wrap justify-center items-center max-w-6xl  mx-auto  my-12 flex-row gap-x-5">
-			{renderedSocialMediaList}
-		</div>
-	);
+  const renderedSocialMediaList = ListedSocialMedias.map((socialMedia) => {
+    const { id, icon, link, name } = socialMedia;
+    return (
+      <SocialMediaItem
+        key={id}
+        img={icon}
+        link={link}
+        name={name}
+      ></SocialMediaItem>
+    );
+  });
+  return (
+    <ul className="flex flex-wrap justify-center items-center max-w-6xl  mx-auto  my-12 flex-row gap-x-5">
+      {renderedSocialMediaList}
+    </ul>
+  );
 };
 
 export default SocialMediaList;
